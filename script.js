e8e8459799a96744f9bd9e8e770b68fefb44d223
Iamronaldosunmu <Iamronaldosunmu@users.noreply.github.com>
2021-06-13 01:57:09 +0100

    Finalized both the top and the bottom navigation bars

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 const loaderAnimationContainer = document.querySelector('#loaderAnimationContainer');
-console.log(loaderAnimationContainer);
+const topNavBar = document.querySelector('.topNavBar');
+const bottomNavBar = document.querySelector('.bottomNavBar');
 setTimeout(loaderAnimationStop, 3500);
 
 function loaderAnimationStop() {
     loaderAnimationContainer.classList.add('hidden');
     setTimeout(() => {
         loaderAnimationContainer.style.display = "none";
+        topNavBar.style.display = "flex";
+        bottomNavBar.style.display = "flex";
     }, 800);
 }
 
