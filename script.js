79e18932ff9c14e650b711458509d0c5ab08bdd0
Ronald Dosunmu <ronaldosunmu@gmail.com>
2021-10-11 11:19:59 +0100

    added the grad pic, the newTweetInputContainer

--- script.js
+++ script.js
@@ -3,6 +3,10 @@
 
 
 //Selecting Elements from the DOM
+const trendingSearchesContainer = document.querySelector('.trendingSearchesContainer');
+const navTwitterStarImage = document.querySelector('.navTwitterStarImage');
+const followMeSection = document.querySelector('.followMeSection');
+const closeFollowMeSection = document.querySelector('.closeFollowMeSection')
 const newTweetInputContainer = document.querySelector('.newTweetInputContainer');
 const closeNewTweetInputContainer = document.querySelector('.closeNewTweetInputContainer');
 const loaderAnimationContainer = document.querySelector('#loaderAnimationContainer');
@@ -36,6 +40,13 @@ const slider = document.querySelector('.sliderContainer .slider');
 const notificationsBody = document.querySelector('.notificationsBody');
 const retweetButtons = document.querySelectorAll('.section--retweet .tweetInteractionSectionButton');
 const likeButtons = document.querySelectorAll('.section--like .tweetInteractionSectionButton');
+const tweetInputSubmitButton = document.querySelector('.tweetInputSubmitButton');
+const tweetInput = document.querySelector('.tweetInput');
+const originalTweet = document.querySelector('.tweet').cloneNode(true);
+const redLikeIcon = '<svg viewBox="0 0 24 24" aria-hidden="true" class="tweetInteractionSectionIcon red"><g><path d="M12 21.638h-.014C9.403 21.59 1.95 14.856 1.95 8.478c0-3.064 2.525-5.754 5.403-5.754 2.29 0 3.83 1.58 4.646 2.73.814-1.148 2.354-2.73 4.645-2.73 2.88 0 5.404 2.69 5.404 5.755 0 6.376-7.454 13.11-10.037 13.157H12z"></path></g></svg>';
+const plainLikeIcon = '<svg viewBox="0 0 24 24" aria-hidden="true" class="tweetInteractionSectionIcon"><g><path d="M12 21.638h-.014C9.403 21.59 1.95 14.856 1.95 8.478c0-3.064 2.525-5.754 5.403-5.754 2.29 0 3.83 1.58 4.646 2.73.814-1.148 2.354-2.73 4.645-2.73 2.88 0 5.404 2.69 5.404 5.755 0 6.376-7.454 13.11-10.037 13.157H12zM7.354 4.225c-2.08 0-3.903 1.988-3.903 4.255 0 5.74 7.034 11.596 8.55 11.658 1.518-.062 8.55-5.917 8.55-11.658 0-2.267-1.823-4.255-3.903-4.255-2.528 0-3.94 2.936-3.952 2.965-.23.562-1.156.562-1.387 0-.014-.03-1.425-2.965-3.954-2.965z"></path></g></svg>';
+const homeBtnEventParameter = {target: document.querySelector('.navBarIconContainer .homeBtn')};
+const tweetOptions = document.querySelectorAll('.tweetOptions');
 
 console.log(retweetButtons, likeButtons);
 
@@ -45,7 +56,7 @@ let selectedTheme = lightingOptionButtonOn;
 let selectedBottomNavButton = homeBtn;
 setTimeout(loaderAnimationStop, 3500);
 
-const arrayOfThingsToChangeInTheme = [body, topNavBar, bottomNavBar, accountInfo, lightsOutContainer, lightsOutPopUp, tweetsContainer];
+const arrayOfThingsToChangeInTheme = [body, topNavBar, bottomNavBar, accountInfo, lightsOutContainer, lightsOutPopUp, tweetsContainer, notificationsBody, messagesContainer, trendingSearchesContainer, followMeSection];
 
 
 let bottomNavBarIcons = [homeBtn, searchBtn, notificationBtn, messageBtn];
@@ -64,10 +75,15 @@ function loaderAnimationStop() {
         newTweetInputContainer.style.display = 'block';
         closeNewTweetInputContainer.style.display = 'block';
         closeNewTweetInputContainer.style.zIndex = '-5';
+        followMeSection.style.display = 'block';
+        closeFollowMeSection.style.display = 'block';
+        closeFollowMeSection.style.zIndex = '-5';
     }, 800);
 }
 
 //Event Listeners
+navTwitterStarImage.addEventListener('click', showFollowMeSection);
+closeFollowMeSection.addEventListener('click', removeCloseFollowMeSection);
 closeNewTweetInputContainer.addEventListener('click', removeNewTweetInput)
 tweetButton.addEventListener('click', showNewTweetInput); //TODO: Create the function
 toggleBtn.addEventListener('click', showAccountInfo);
@@ -78,13 +94,38 @@ closeLightsOutPopUp.addEventListener('click', closeLightingSection);
 lightsOutContainer.addEventListener('click', selectaButton );
 allButton.addEventListener('click', selectANotificationsNavButton);
 mentionsButton.addEventListener('click', selectANotificationsNavButton);
-for (item of retweetButtons) {
-    item.addEventListener('click', retweetClicked);
+tweetInputSubmitButton.addEventListener('click', handleSubmit);
+for (let retweetButton of retweetButtons) {
+    retweetButton.addEventListener('click', retweetClicked);
+}
+for (let likeButton of likeButtons) {
+    likeButton.addEventListener('click', likeClicked);
+}
+for (let tweetOption of tweetOptions) {
+    tweetOption.addEventListener('click', showFollowMeSection);
 }
 //Event Listener Functions
+function likeClicked(e){
+    let likeNo = this.nextElementSibling;
+    likeNo.classList.toggle('clicked');
+    let tweetInteractionSectionButton = this;
+    tweetInteractionSectionButton.classList.toggle('clicked');
+    tweetInteractionSectionButton.innerHTML = [...tweetInteractionSectionButton.classList].includes('clicked') ? redLikeIcon : plainLikeIcon;
+
+    // tweetInteractionSectionButton.innerHTML = [...tweetInteractionSectionButton.classList].includes('clicked') ? redLikeIcon : plainLikeIcon;
+
+
+    if ([...tweetInteractionSectionButton.classList].includes('clicked')){
+        likeNo.innerText = +likeNo.innerText + 1;
+    }
+    else {
+        likeNo.innerText = +likeNo.innerText - 1;
+    }
+}
 function retweetClicked(e) {
     this.querySelector('.tweetInteractionSectionIcon').classList.toggle('clicked');
     let retweetNo = this.nextElementSibling;
+    retweetNo.classList.toggle('clicked');
     
     if ([...this.querySelector('.tweetInteractionSectionIcon').classList].includes('clicked')){
         retweetNo.innerText = +retweetNo.innerText + 1;
@@ -175,6 +216,7 @@ function changeBodyTo(nextDisplay) {
         navText.style.display = 'none';
         navSearch.style.display = 'none';
         messagesContainer.style.display = 'none';
+        trendingSearchesContainer.style.display = 'none';
         tweetsContainer.style.display = 'block';
         navTwitterImage.style.display = 'block';
         notificationsContainer.style.display = 'none';
@@ -188,7 +230,7 @@ function changeBodyTo(nextDisplay) {
         navSearch.style.display = 'block';
         navSearch.setAttribute('placeholder', 'Search RedTwitter');
         notificationsContainer.style.display = 'none';
-
+        trendingSearchesContainer.style.display = 'block';
 
     }
     else if (nextDisplay == messageBtn) {
@@ -200,6 +242,7 @@ function changeBodyTo(nextDisplay) {
         messagesContainer.style.display = 'block';
        navSearch.setAttribute('placeholder', 'Search for people and groups');
        notificationsContainer.style.display = 'none';
+        trendingSearchesContainer.style.display = 'none';
 
     }
     else if (nextDisplay == notificationBtn) {
@@ -209,6 +252,7 @@ function changeBodyTo(nextDisplay) {
         tweetsContainer.style.display = 'none';
         navText.style.display = 'block';
        notificationsContainer.style.display = 'block';
+       trendingSearchesContainer.style.display = 'none';
     }
 }
 
@@ -255,6 +299,13 @@ function showNewTweetInput(e) {
     closeNewTweetInputContainer.style.zIndex = '4';
     newTweetInputContainer.classList.add('visible');
     closeNewTweetInputContainer.classList.add('visible');
+    tweetInput.focus();
+    tweetInputSubmitButton.disabled = false;
+}
+function showFollowMeSection(e) {
+    closeFollowMeSection.style.zIndex = '4';
+    followMeSection.classList.add('visible');
+    closeFollowMeSection.classList.add('visible');
 }
 function removeNewTweetInput(e) {
     newTweetInputContainer.classList.remove('visible');
@@ -263,4 +314,64 @@ function removeNewTweetInput(e) {
         closeNewTweetInputContainer.style.zIndex = '-5';
     }, 500);
 }
+function removeCloseFollowMeSection (e) {
+    followMeSection.classList.remove('visible');
+    closeFollowMeSection.classList.remove('visible');
+    setTimeout( () => {
+        closeFollowMeSection.style.zIndex = '-5';
+    }, 500);
+}
+function handleSubmit(e) {
+    e.preventDefault();
+    const tweetContent = tweetInput.value;
+    tweetInput.value = '';
+    removeNewTweetInput();
+    tweetInputSubmitButton.disabled = true;
+    const tweetObject = new Tweet(tweetContent, 0, 0, 0);
+    const tweet = tweetObject.returnTweetElement();
+    tweetsContainer.insertBefore(tweet, tweetsContainer.firstChild);
+    changeBodyTo(homeBtn);
+    toggleSelectedClass(homeBtnEventParameter);
+    
+
+}
 // TODO: Style the tweet input container and handle the event to make it show when the tweetButton is clicked.
+
+class Tweet {
+    constructor(content, commentNo, retweetNo, likeNo) {
+        this.content = content;
+        this.commentNo = commentNo;
+        this.retweetNo = retweetNo;
+        this.likeNo = likeNo;
+    }
+    returnTweetElement () {
+        const {content, commentNo, retweetNo, likeNo} = this;
+        // const originalTweet = document.querySelector('.tweet'); 
+        const tweet = originalTweet.cloneNode(true);
+        const likeButton = tweet.querySelector('.section--like .tweetInteractionSectionButton');
+        likeButton.addEventListener('click', likeClicked);
+        const tweetOption = tweet.querySelector('.tweetOption');
+        tweetOption.addEventListener('clicke', showFollowMeSection);
+        const retweetButton = tweet.querySelector('.section--retweet .tweetInteractionSectionButton');
+        retweetButton.addEventListener('click', retweetClicked);
+        const tweetContent = tweet.querySelector('.tweetContentText');
+        tweetContent.innerText = content;
+        const tweetCommentNo = tweet.querySelector('.commentNo');
+        tweetCommentNo.innerText = commentNo;
+        const tweetRetweetNo = tweet.querySelector('.retweetNo');
+        tweetRetweetNo.innerText = retweetNo;
+        const tweetLikeNo = tweet.querySelector('.likeNo');
+        tweetLikeNo.innerText = likeNo;
+
+        return tweet;
+
+    }
+}
+//TODO: Implement Light mode for the follow me section pop up . DONE .
+//TODO: Note that the loop for the tweet option buttons may be slowing down the code . DONE .
+//TODO: Make the options button on all the tweet to open the follow me section . DONE .
+//TODO: Style the tweets that will appear in the notifications container
+//TODO: Implement Infinite Scroll in the tweets Container
+//TODO: Implement Filter on the trending Searches and the current messages whenever something is typed into the input field
+//TODO: Try to ensure that the layout looks good on all devices
+//TODO: Don't forget the layout problem on bigger screens
